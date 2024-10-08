cafed59b36d4ef12a79f37841ac7eb751bddd56a
enzomealla <enzomealla@hotmail.com>
2024-10-08 20:17:07 +0000

    Se realizo punto A

---
+++ RTA_SCRIPT_Examen_20241008/Punto_A.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+cd -
+
+mkdir -p RTA_ARCHIVOS_Examen_20241008/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores} | tree RTA_ARCHIVOS_Examen_20241008/
+
+
+done
+
+
+
