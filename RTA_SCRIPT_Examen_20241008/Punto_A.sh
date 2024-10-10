5478a4e00e1179feb0d83b857c447e2e410a94f4
root <root@enzomealla>
2024-10-10 02:39:01 +0000

    se arreglo la creacion de la estructura en el directorio raiz

--- RTA_SCRIPT_Examen_20241008/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_A.sh
@@ -1,9 +1,8 @@
 
 #!/bin/bash
 
-cd -
 
-mkdir -p RTA_ARCHIVOS_Examen_20241008/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores} | tree RTA_ARCHIVOS_Examen_20241008/
+sudo mkdir -p /home/repogit/UTNFRA_SO_1P2C_2024_Mealla/RTA_ARCHIVOS_Examen_20241008/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
 
 
 done
