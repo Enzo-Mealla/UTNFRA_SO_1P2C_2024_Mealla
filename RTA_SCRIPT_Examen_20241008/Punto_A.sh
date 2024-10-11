04d4033ac675de15b1b94003a74c4d09e5b13d6c
}Enzo-Mealla <enzomealla@hotmail.com>
2024-10-11 17:55:43 +0000

    Arreglado, terminado punto A.

--- RTA_SCRIPT_Examen_20241008/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_A.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 
-sudo mkdir -p /home/repogit/UTNFRA_SO_1P2C_2024_Mealla/RTA_ARCHIVOS_Examen_20241008/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
+sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
 
 
 done
